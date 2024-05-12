017804d1007089d5fc11e4993f3c241f9be5d93c
kimjunhonumber <168261689+kimjunhonumber@users.noreply.github.com>
2024-05-13 03:48:32 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,7 +12,7 @@ st.markdown("""
 """)
 
 # 컬럼으로 레이아웃 구성
-col1, col2 = st.columns(2)
+col1 st.columns(2)
 
 with col1:
     st.subheader('1. 생각 톡톡 AI')
@@ -26,19 +26,16 @@ with col1:
     link_url = 'https://www.naver.com/'  # 이미지를 클릭했을 때 이동할 링크
     st.markdown(f'<a href="{link_url}" target="_blank"><img src="{image_path}" alt="도덕 교육 이미지" style="width:100%;"></a>', unsafe_allow_html=True)
 
-with col2:
-    st.subheader('2. 발표문 생성 보조도구')
-    st.write('발표 준비에 도움이 필요하신가요? 이 도구가 도와드립니다.')
 
 col3, col4 = st.columns(2)
 
-with col3:
-    st.subheader('3. 안건 제안 보조 챗봇 & 보조도구')
-    st.write('보조챗봇을 통해 법률안과 제안서에 대한 이해를 높일 수 있습니다. 챗봇과의 대화를 통해 아이디어를 만들어보세요.')
-    st.write('보조도구를 통해 법률안을 만들 수 있습니다.')
+with col2:
+    st.subheader('2.마음톡톡 AI')
+    st.write('도덕적 딜레마상황에 대해서 생각해 보고 어떤 선택기 옮은 것인지 생각해 볼 수 있습니다.')
+   
 
-with col4:
-    st.subheader('4. 이미지 생성 보조도구')
+with col3:
+    st.subheader('3.실천톡톡 AI')
     st.write('텍스트 설명을 바탕으로 이미지를 생성합니다.')
     st.markdown('**주의:** 이미지 생성요청이 한번에 몰릴 경우 이미지생성오류가 있을 수 있습니다.')
 
@@ -48,6 +45,3 @@ st.markdown("""
     왼쪽의 탐색 바(> 클릭)를 사용하여 원하는 도구를 선택하고 사용해 보세요. 각 도구는 사용자의 입력에 따라 다양한 결과를 제공합니다.
 """)
 
-st.markdown("""
-    ## 비밀번호는 선생님에게 물어보세요.
-""")
