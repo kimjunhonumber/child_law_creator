973e766da653b92f1fb5e77518854b1c5e8211d9
kimjunhonumber <168261689+kimjunhonumber@users.noreply.github.com>
2024-05-13 04:26:44 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,39 +1,44 @@
 import streamlit as st
-from PIL import Image
 
 # 홈 페이지의 타이틀 설정
-st.title('🛠 도덕 AI')
+st.title('🛠 자치활동 보조 인공지능도구 모음 홈페이지')
+
 # 애플리케이션 소개
 st.markdown("""
-## 🌟 안녕하세요!
-이 앱은 미덕과 관련한 학습을 하기 위해서 제작되어 있습니다. 
+    ## 🌟 안녕하세요!
+    이 애플리케이션은 학생 자치활동에 필요한 여러 가지 유용한 도구들로 구성되어 있습니다. 아래는 제작된 도구들의 리스트와 기능 설명입니다.
 """)
+
 # 컬럼으로 레이아웃 구성
-col1, col2 =st.columns(2)
+col1, col2 = st.columns(2)
+
 with col1:
-    st.subheader('1.마음AI)
-    st.write('이 도구를 사용하면, 마음에 대해 알 수 있습니다.')
-    Image_path = 'https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/xSO/image/LlSElL_exe7lc0mLeGSxBr4EZjg.png'  # 로컬 파일 경로 또는 URL
-    st.image(image_path, caption='도덕 교육 이미지')  # 이미지에 설명 추가
+    st.subheader('1. 제안서 생성 보조도구')
+    st.write('이 도구를 사용하면, 제안서를 쉽고 빠르게 작성할 수 있습니다.')
+
 with col2:
-    st.subheader()
-    st.write()
-col3, col4 =st.columns(2)
+    st.subheader('2. 발표문 생성 보조도구')
+    st.write('발표 준비에 도움이 필요하신가요? 이 도구가 도와드립니다.')
+
+col3, col4 = st.columns(2)
+
 with col3:
-    st.subheader('2. 생각AI')
-    st.write('도덕적 생각을 하는 AI봇입니다.')
-    st.write('도덕적 생각을 하는 AI봇입니다.')
-    image_path = 'https://postfiles.pstatic.net/MjAyNDA1MTNfMjQz/MDAxNzE1NTM5ODc2OTY3.8AWJDUJhCOJe2ycZlMbIUy2tm38ZKIhOywDeYMHcMDMg.Robn_xaq42P3a3iLkvXvS_EaUP_tFJr2OMEawP1hA7gg.JPEG/DALL%C2%B7E_2024-05-13_03.50.30_-_A_colorful_illustration_showing_a_group_of_dive.jpg?type=w773'
-    st.image(image_path, caption='도덕 교육 이미지')  # 이미지에 설명 추가 
+    st.subheader('3. 안건 제안 보조 챗봇 & 보조도구')
+    st.write('보조챗봇을 통해 법률안과 제안서에 대한 이해를 높일 수 있습니다. 챗봇과의 대화를 통해 아이디어를 만들어보세요.')
+    st.write('보조도구를 통해 법률안을 만들 수 있습니다.')
 
 with col4:
-    st.subheader('3. 실천AI')
-    st.write('실천을 하는 AI봇입니다.')
-    st.markdown('')
+    st.subheader('4. 이미지 생성 보조도구')
+    st.write('텍스트 설명을 바탕으로 이미지를 생성합니다.')
+    st.markdown('**주의:** 이미지 생성요청이 한번에 몰릴 경우 이미지생성오류가 있을 수 있습니다.')
+
 # 추가적인 정보 제공
 st.markdown("""
-## 🚀 시작하기
-왼쪽의 탐색 바(> 클릭)를 사용하여 원하는 도구를 선택하고 사용해 보세요. 각 도구는 사용자의 입력에 따라 다양한 결과를 제공합니다.
+    ## 🚀 시작하기
+    왼쪽의 탐색 바(> 클릭)를 사용하여 원하는 도구를 선택하고 사용해 보세요. 각 도구는 사용자의 입력에 따라 다양한 결과를 제공합니다.
 """)
 
+st.markdown("""
+    ## 비밀번호는 선생님에게 물어보세요.
+""")
 
