87d02d4e3dacad4af60aa57d04d73d5a833d1be9
kimjunhonumber <168261689+kimjunhonumber@users.noreply.github.com>
2024-05-13 04:06:54 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,9 +12,8 @@ st.markdown("""
 """)
 
 # 컬럼으로 레이아웃 구성
-col1 st.columns(1)
 
-with col1:
+ col1:
     st.subheader('1. 생각 톡톡 AI')
     st.write('이 도구를 사용하면, 여러분이 배워야 할 도덕적 덕목에 대해서 잘 알 수 있습니다.')
     st.write('이렇게 질문하세요. 예) 정의로운 삶은 뜻은?')
@@ -27,18 +26,14 @@ with col1:
     st.markdown(f'<a href="{link_url}" target="_blank"><img src="{image_path}" alt="도덕 교육 이미지" style="width:100%;"></a>', unsafe_allow_html=True)
 
 
-col2 = st.columns(1)
-
-with col2:
+ col2:
     st.subheader('2.마음톡톡 AI')
     st.write('도덕적 딜레마상황에 대해서 생각해 보고 어떤 선택기 옮은 것인지 생각해 볼 수 있습니다.'
     image_path = 'https://postfiles.pstatic.net/MjAyNDA1MTNfMjQz/MDAxNzE1NTM5ODc2OTY3.8AWJDUJhCOJe2ycZlMbIUy2tm38ZKIhOywDeYMHcMDMg.Robn_xaq42P3a3iLkvXvS_EaUP_tFJr2OMEawP1hA7gg.JPEG/DALL%C2%B7E_2024-05-13_03.50.30_-_A_colorful_illustration_showing_a_group_of_dive.jpg?type=w773'
     st.image(image_path, caption='도덕 교육 이미지')  # 이미지에 설명 추가 
 
 
-col2 = st.columns(1)
-
-with col2:
+ col3:
     st.subheader('3.실천톡톡 AI')
     st.write('텍스트 설명을 바탕으로 이미지를 생성합니다.')
     st.markdown('**주의:** 이미지 생성요청이 한번에 몰릴 경우 이미지생성오류가 있을 수 있습니다.')
